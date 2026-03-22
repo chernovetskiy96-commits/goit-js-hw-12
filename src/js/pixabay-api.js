72f01fff0cedf1009cacc22db4d2007d782284a3
Yevhenii Chernov <yevheniichernov@MacBook-Air-de-Yevhenii.local>
2026-03-22 18:46:01 +0200

    start hw 12

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,5 @@
+
+
+function getImagesByQuery(query, page) {
+    
+}
---
+++ src/js/render-functions.js
@@ -0,0 +1,38 @@
+import SimpleLightbox from "simplelightbox";
+import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm"
+
+
+const gallery = document.querySelector('.gallery');
+
+
+const litebox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250
+});
+
+export function createGallery(images) {
+    const markup = images
+        .map({
+             webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+        }) => `<li class="gallery-item">
+          <a class="gallery-link" href="${largeImageURL}">
+            <img
+              class="gallery-image"
+              src="${webformatURL}"
+              alt="${tags}"
+            />
+          </a>
+          <div class="info">
+            <p class="info-item"><b>Likes</b> ${likes}</p>
+            <p class="info-item"><b>Views</b> ${views}</p>
+            <p class="info-item"><b>Comments</b> ${comments}</p>
+            <p class="info-item"><b>Downloads</b> ${downloads}</p>
+          </div>
+        </li>`
+}
